b76d83c35c46822a8c9432c6273075de399224e1
Minh Luy <vominhluy1@gmail.com>
2020-07-04 11:39:11 +0700

    Luy thay doi index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Trang chủ</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="row" style="background-color: #fed014;" >
+		<div class="col-sm-10 col-md-9" style="text-align: center;">
+			
+		</div>
+		<div class="col-sm-2 col-md-3" style="text-align: center; margin-top: 20px;">
+			<a href="php/login.php" title="">Đăng Nhập</a>
+		</div>
+	</div>
+</body>
+</html>
